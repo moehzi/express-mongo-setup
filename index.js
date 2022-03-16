983809bcc62f4419394d99bc3c541408e43a5409
moehzi <faizi1234098@gmail.com>
2022-03-16 16:51:14 +0800

    feat: add simple logger middleware

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const router = require('./routers');
 const app = express();
 const port = 3000;
 
+const myLogger = (req, res, next) => {
+  console.log('Logger');
+  next();
+};
+
+app.use(myLogger);
+
 app.use(router);
 
 app.listen(port, () => {
