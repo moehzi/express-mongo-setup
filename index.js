5d234f905f93403bda9eef0e7fb13091f78a1379
moehzi <faizi1234098@gmail.com>
2022-03-16 16:33:14 +0800

    feat: put,post and delet method

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.post('/users', (req, res) => {
+  console.log('post');
+  res.send('Got a POST request');
+});
+
+app.put('/users', (req, res) => {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/users', (req, res) => {
+  res.send('Got a Delete request at /user');
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
