94429441a73aeb16f3cb9739d19349121259404f
moehzi <faizi1234098@gmail.com>
2022-03-20 11:40:39 +0800

    feat: get all /users method with User model

--- routers.js
+++ routers.js
@@ -2,11 +2,23 @@ const express = require('express');
 const { ObjectId } = require('mongodb');
 const router = express.Router();
 const connection = require('./connection.js');
+
+require('./mongoose');
+
 const User = require('./User');
-require('mongoose');
 
 router.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.send({ data: users });
+  } catch (err) {
+    res.send({ message: err.message || 'Internal Server Errror' });
+  }
 });
 
 module.exports = router;
